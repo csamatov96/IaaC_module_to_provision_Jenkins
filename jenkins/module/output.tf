854e764f87e900aa149b90aa8e6065797fdff340
csamatov96 <csamatov96@gmail.com>
2020-02-14 23:58:14 -0600

    added

--- jenkins/module/output.tf
+++ jenkins/module/output.tf
@@ -1,13 +1,13 @@
 output "jenkins_master" {
-  value = " http://${aws_route53_record.jenkins_master.name}:8080"
+  value = " http://${aws_route53_record.jenkins_master.name}:9090"
 }
 
 output "worker1" {
-  value = " http://${aws_route53_record.jenkins_worker1.name}:8080"
+  value = " http://${aws_route53_record.jenkins_worker1.name}:9090"
 }
 
 output "worker2" {
-  value = " http://${aws_route53_record.jenkins_worker2.name}:8080"
+  value = " http://${aws_route53_record.jenkins_worker2.name}:9090"
 }
 /*
 output "worker3" {
--- jenkins/module/sec_group.tf
+++ jenkins/module/sec_group.tf
@@ -3,21 +3,34 @@ resource "aws_security_group" "allow_ssh_and_jenkins" {
   description = "Allow SSH and jenkins"
   vpc_id      = "${var.vpc_id}"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
+
 }
 
-
-resource "aws_security_group_rule" "allow_all_ingress" {
-  type            = "ingress"
-  from_port       = 0
-  to_port         = 65535
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]                            
-  security_group_id = "${aws_security_group.allow_ssh_and_jenkins.id}"
-}
